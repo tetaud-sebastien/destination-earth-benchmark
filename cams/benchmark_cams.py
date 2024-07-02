6927abf19d47c627229d04cdca6b6ed9b8d891db
Ilja Skrypnyk <ilja.skrypnyk@esa.int>
2024-07-02 15:29:16 +0000

    Styling changes to make code compliant with flake8

--- cams/benchmark_cams.py
+++ cams/benchmark_cams.py
@@ -11,7 +11,7 @@ from utils import (CamsERA5, ParticleVisualizer, load_config, plot_benchmark,
 if __name__ == "__main__":
 
     dir_path = os.path.dirname(os.path.realpath(__file__))
-    config = load_config(file_path=os.path.join(dir_path,"config.yaml"))
+    config = load_config(file_path=os.path.join(dir_path, "config.yaml"))
 
     # Dictionary to store benchmarking results
 
@@ -27,18 +27,24 @@ if __name__ == "__main__":
     request_issues = 0
 
     benchmark = {
-            "download_time": [None]* num_requests,
-            "data_processing": [None]* num_requests,
-            "animation": [None]* num_requests,
-            "end_to_end": [None]* num_requests,
-            "request_issues": [None]* num_requests,
+            "download_time": [None] * num_requests,
+            "data_processing": [None] * num_requests,
+            "animation": [None] * num_requests,
+            "end_to_end": [None] * num_requests,
+            "request_issues": [None] * num_requests,
             }
 
     query = config["cams_request"]
     cams = CamsERA5()
 
     # Repeat benchmarking for a specified number of requests
-    for r in tqdm(range(num_requests), desc="Processing requests", unit="request", ncols=100, colour="#3eedc4"):
+    for r in tqdm(
+        range(num_requests),
+        desc="Processing requests",
+        unit="request",
+        ncols=100,
+        colour="#3eedc4"
+    ):
 
         t0 = time.time()
         try:
@@ -52,13 +58,13 @@ if __name__ == "__main__":
         ds = cams.process()
         t2 = time.time()
         pm10_anim = ParticleVisualizer.generate_animation(ds)
-        
+
         t3 = time.time()
         # Record benchmarking times
-        benchmark["download_time"][r]=(t1-t0)
-        benchmark["data_processing"][r]=(t2-t1)
-        benchmark["animation"][r]=(t3-t2)
-        benchmark["end_to_end"][r]=(t3-t0)
+        benchmark["download_time"][r] = (t1 - t0)
+        benchmark["data_processing"][r] = (t2 - t1)
+        benchmark["animation"][r] = (t3 - t2)
+        benchmark["end_to_end"][r] = (t3 - t0)
         benchmark["request_issues"][r] = request_issues
 
         # Cleanup by deleting zip and nc files
@@ -69,27 +75,11 @@ if __name__ == "__main__":
 
     title = 'End to End CAMS particle density animation generation benchmark'
     plot_benchmark(benchmark_dict=benchmark,
-                   out_dir=out_dir,title=title)
+                   out_dir=out_dir,
+                   title=title)
 
-    filename = os.path.join(out_dir,"cams_benchmark.json")
-    save_results(data=benchmark,filename=filename)
-    logger.info(f"Benchmark completed. Results saved to {out_dir}", "benchmarks.json")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    filename = os.path.join(out_dir, "cams_benchmark.json")
+    save_results(data=benchmark, filename=filename)
+    logger.info(
+        f"Benchmark completed. Results saved to {out_dir}",
+        "benchmarks.json")
